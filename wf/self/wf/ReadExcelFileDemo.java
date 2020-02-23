bee991c0194481ec1a5d5921ba55849d772327cb
Jyoti Gill <jyotigill089@gmail.com>
2020-02-23 15:44:19 -0800

    xlsx read working with XSSFWorkbook & XSSFSheet

---
+++ wf/self/wf/ReadExcelFileDemo.java
@@ -0,0 +1,58 @@
+package wf;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadExcelFileDemo {
+	public static void main(String args[]) throws IOException {
+		File myFile = new File("/Users/jyotigill/code/ApachePOI_jar/test_apache_poi.xlsx");
+        FileInputStream fis = new FileInputStream(myFile);
+
+        // Finds the workbook instance for XLSX file
+        XSSFWorkbook myWorkBook = new XSSFWorkbook (fis);
+       
+        // Return first sheet from the XLSX workbook
+        XSSFSheet mySheet = myWorkBook.getSheetAt(0);
+       
+        // Get iterator to all the rows in current sheet
+        Iterator<Row> rowIterator = mySheet.iterator();
+       
+        // Traversing over each row of XLSX file
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+
+            // For each row, iterate through each columns
+            Iterator<Cell> cellIterator = row.cellIterator();
+            while (cellIterator.hasNext()) {
+
+                Cell cell = cellIterator.next();
+
+                switch (cell.getCellType()) {
+                case Cell.CELL_TYPE_STRING:
+                    System.out.print(cell.getStringCellValue() + "\t");
+                    break;
+                case Cell.CELL_TYPE_NUMERIC:
+                    System.out.print(cell.getNumericCellValue() + "\t");
+                    break;
+                case Cell.CELL_TYPE_BOOLEAN:
+                    System.out.print(cell.getBooleanCellValue() + "\t");
+                    break;
+                default :
+             
+                }
+            }
+            System.out.println("");
+        }
+	}
+}
